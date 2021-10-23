0e2ef4460747acaedd70ddb6511073baf3585029
jasazupancic123 <93047058+jasazupancic123@users.noreply.github.com>
2021-10-23 18:22:30 +0200

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Že programiram" << endl
+		<< "Tole je naslednja vrstica." << endl; // s "endl;" zaključimo vrstico
+	return 0;
+}
